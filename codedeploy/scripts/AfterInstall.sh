2cd1eb5e83d5f9e31a8d2d71727cb98858fe3031
Fayas Akram <fayas@cinergix.com>
2024-03-11 18:00:13 +0530

    initial commit

---
+++ codedeploy/scripts/AfterInstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# run pre-deployment tasks for app
+
+# stop the app
+systemctl start my-java-app.service
+
+exit 0
---
+++ codedeploy/scripts/BeforeInstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# run pre-deployment tasks for app
+
+# stop the app
+systemctl stop my-java-app.service
+
+exit 0
